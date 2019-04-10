13623bcb1eedf317d7cca369695e3dac7775071a
marcovazquez <marcovazquez77@hotmail.com>
2019-04-10 18:01:53 -0500

    Project init

---
+++ yorha-client-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yorha-client-ios
+//
+//  Created by Marco Vazquez on 4/10/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
