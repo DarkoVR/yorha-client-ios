809e035bfb27c873f69dc0f9285e67a5fe6dde33
marcovazquez <marcovazquez77@hotmail.com>
2019-04-28 19:31:53 -0500

    Constants class created in order to replace the Endpoints class

---
+++ yorha-client-ios/DataManager/Common/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Endpoints.swift
+//  yorha-client-ios
+//
+//  Created by Marco Vazquez on 4/27/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import Foundation
+
+enum Endpoints {
+    static let automatas = "/automatas"
+    static let bosses = "/bosses"
+    static let podPrograms = "/podprograms"
+    static let weapons = "/weapons"
+}
+
+enum Methods {
+    static let get = "GET"
+    static let post = "POST"
+    static let put = "PUT"
+    static let delete = "DELETE"
+}
