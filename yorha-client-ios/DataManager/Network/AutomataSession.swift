1847bf14bac9f1d445a1bf00fda28da39664f43f
marcovazquez <marcovazquez77@hotmail.com>
2019-04-29 14:52:32 -0500

    All http methods for automata added

--- yorha-client-ios/DataManager/Network/AutomataSession.swift
+++ yorha-client-ios/DataManager/Network/AutomataSession.swift
@@ -11,7 +11,9 @@ import Foundation
 class AutomataSession {
     
     func retrieveAutomatas() {
-        let task = URLSession.shared.dataTask(with: request(endpoint: Endpoints.automatas)) {
+        let task = URLSession.shared.dataTask(
+            with: request(endpoint: Endpoints.automatas)
+        ) {
             data, response, error in
             
             guard let data = data, error == nil else {
@@ -24,6 +26,82 @@ class AutomataSession {
             } else {
                 print("somenthing went wrong!")
             }
+        }
+        task.resume()
+    }
+    
+    func retrieveOneAutomata(automata: Automata) {
+        let task = URLSession.shared.dataTask(
+            with: request(endpoint: Endpoints.automatas, id: automata.ID)
+        ) {
+            data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            if let jsonPetitions = try? JSONDecoder().decode(Automata.self, from: data) {
+                print(jsonPetitions)
+            } else {
+                print("somenthing went wrong!")
+            }
+        }
+        task.resume()
+    }
+    
+    func storeAutomata(automata: Automata){
+        guard let uploadData = try? JSONEncoder().encode(automata) else {
+            return
+        }
+        
+        let task = URLSession.shared.uploadTask(
+            with: request(endpoint: Endpoints.automatas, method: Methods.post), from: uploadData
+        ) {
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Automata stored!")
+        }
+        task.resume()
+    }
+    
+    func updateAutomata(automata: Automata) {
+        guard let uploadData = try? JSONEncoder().encode(automata) else {
+            return
+        }
+        
+        let task = URLSession.shared.uploadTask(
+            with: request(endpoint: Endpoints.automatas, id: automata.ID, method: Methods.put), from: uploadData
+        ) {
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Automata updated!")
+        }
+        task.resume()
+    }
+    
+    func deleteAutomata(automata: Automata) {
+        let task = URLSession.shared.dataTask(
+            with: request(endpoint: Endpoints.automatas, id: automata.ID, method: Methods.delete)
+        ){
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Automata deleted!")
         }
         task.resume()
     }
--- yorha-client-ios/ViewController.swift
+++ yorha-client-ios/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //let automata = Automata(ID: 4, Name: "New automata", Occupation: "Exists", Photo: "3.png", RaceID: 1, Race: Race(ID: 400, Name: "Human"))
+        //let automataPost = Automata(ID: 0, Name: "New automata", Occupation: "Exists", Photo: "1.png", RaceID: 1, Race: Race())
+        //let automataPut = Automata(ID: 32, Name: "New automata x2", Occupation: "Exists x2", Photo: "1.png x2", RaceID: 2, Race: Race())
+        //let automataDelete = Automata(ID: 31, Name: "New automata x2", Occupation: "Exists x2", Photo: "1.png x2", RaceID: 2, Race: Race())
         AutomataSession.init().retrieveAutomatas()
+        //AutomataSession.init().retrieveOneAutomata(automata: automata)
+        //AutomataSession.init().storeAutomata(automata: automataPost)
+        //AutomataSession.init().updateAutomata(automata: automataPut)
+        //AutomataSession.init().deleteAutomata(automata: automataDelete)
     }
 
     @IBAction func refresh(_ sender: UIRefreshControl) {
