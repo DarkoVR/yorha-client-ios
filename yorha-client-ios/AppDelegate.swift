764a7d8f243bdbe89680f6588afe8128708d621e
marcovazquez <marcovazquez77@hotmail.com>
2019-05-02 21:56:03 -0500

    View initialization for automata module

--- yorha-client-ios/AppDelegate.swift
+++ yorha-client-ios/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let automataModule = AutomataRouter.createModule()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = automataModule
+        window?.makeKeyAndVisible()
         return true
     }
 
