69196c5d765a24a7fea519de3edc197e42679c34
marcovazquez <marcovazquez77@hotmail.com>
2019-05-03 22:08:07 -0500

    Automata Router adapted

--- yorha-client-ios/Modules/Automatas/AutomataProtocols.swift
+++ yorha-client-ios/Modules/Automatas/AutomataProtocols.swift
@@ -6,25 +6,57 @@
 //  Copyright © 2019 Marco Vazquez. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 //MARK: Wireframe -
 protocol AutomataWireframeProtocol: class {
-
+    static func createModule() -> UIViewController
 }
 //MARK: Presenter -
 protocol AutomataPresenterProtocol: class {
-
+    var view: AutomataViewProtocol? { get set }
+    var interactor: AutomataInteractorProtocol? { get set }
+    var wireFrame: AutomataWireframeProtocol? { get set }
+    func viewDidLoad()
+    func viewDidRefresh()
+    func showDetailScreen(_ data: Automata)
+    func didRetrieveData(_ data: [Automata])
+    func onError()
+    func isInternetConnected() -> Bool
 }
 
 //MARK: Interactor -
 protocol AutomataInteractorProtocol: class {
-
-  var presenter: AutomataPresenterProtocol?  { get set }
+    var presenter: AutomataPresenterProtocol?  { get set }
+    var remoteData: AutomataRemoteDataProtocol?  { get set }
+    var localData: AutomataLocalDataProtocol?  { get set }
+    func retrieveData()
+    func onPostsRetrieved(_ data: [Automata])
+    func onError()
 }
 
 //MARK: View -
 protocol AutomataViewProtocol: class {
+    var presenter: AutomataPresenterProtocol?  { get set }
+    func showData(with data: [Automata])
+    func showError()
+    func showLoading()
+    func hideLoading()
+    func isInternetConnected() -> Bool
+}
 
-  var presenter: AutomataPresenterProtocol?  { get set }
+//Data Manager protocols
+protocol AutomataRemoteDataProtocol: class {
+    var interactor: AutomataInteractorProtocol?  { get set }
+    var presenter: AutomataPresenterProtocol?  { get set }
+    func retrieveAutomatas()
+    func retrieveOneAutomata(automata: Automata)
+    func storeAutomata(automata: Automata)
+    func updateAutomata(automata: Automata)
+    func deleteAutomata(automata: Automata)
+}
+
+protocol AutomataLocalDataProtocol: class {
+    func retrieveAutomatas() throws -> [AutomataCD]
+    func storeAutomata(id: Int32, name: String, occupation: String, photo: String, raceID: Int32, raceName: String) throws
 }
