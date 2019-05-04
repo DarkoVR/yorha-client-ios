0f8d02eeb031ea7d0dbc4cd82bf2ea4876ae0a90
marcovazquez <marcovazquez77@hotmail.com>
2019-05-03 22:07:01 -0500

    Automata Interactor adapted

--- yorha-client-ios/Modules/Automatas/AutomataInteractor.swift
+++ yorha-client-ios/Modules/Automatas/AutomataInteractor.swift
@@ -9,6 +9,62 @@
 import UIKit
 
 class AutomataInteractor: AutomataInteractorProtocol {
-
     weak var presenter: AutomataPresenterProtocol?
+    var remoteData: AutomataRemoteDataProtocol?
+    var localData: AutomataLocalDataProtocol?
+    
+    func onError() {
+        presenter?.onError()
+    }
+    
+    func retrieveData() {
+        do {
+            if let data = try localData?.retrieveAutomatas() {
+                let automataList = data.map() {
+                    return Automata(
+                        ID: Int($0.id),
+                        Name: $0.name!,
+                        Occupation: $0.occupation!,
+                        Photo: $0.photo!,
+                        RaceID: Int($0.raceID),
+                        Race: Race(
+                            ID: Int($0.raceID),
+                            Name: $0.name
+                        )
+                    )
+                }
+                
+                if (presenter?.isInternetConnected())! || automataList.isEmpty {
+                    remoteData?.retrieveAutomatas()
+                }else{
+                    print("Local data for Automatas retrieved")
+                    presenter?.didRetrieveData(automataList)
+                }
+            } else {
+                remoteData?.retrieveAutomatas()
+            }
+            
+        } catch {
+            presenter?.didRetrieveData([])
+        }
+    }
+    
+    func onPostsRetrieved(_ data: [Automata]) {
+        presenter?.didRetrieveData(data)
+        
+        for automata in data {
+            do {
+                try localData?.storeAutomata(
+                    id: Int32(automata.ID),
+                    name: automata.Name,
+                    occupation: automata.Occupation,
+                    photo: automata.Photo,
+                    raceID: Int32(automata.RaceID),
+                    raceName: automata.Race.Name!
+                )
+            } catch {
+                
+            }
+        }
+    }
 }
