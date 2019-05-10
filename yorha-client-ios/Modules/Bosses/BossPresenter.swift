16d49dae672076329ec673ce7fc1ea2bf03fabb7
marcovazquez <marcovazquez77@hotmail.com>
2019-05-09 23:32:42 -0500

    Updated: Boss presenter added

--- yorha-client-ios/Modules/Bosses/BossPresenter.swift
+++ yorha-client-ios/Modules/Bosses/BossPresenter.swift
@@ -9,15 +9,41 @@
 import UIKit
 
 class BossPresenter: BossPresenterProtocol {
-
-    weak private var view: BossViewProtocol?
+    var router: BossRouterProtocol?
+    weak internal var view: BossViewProtocol?
     var interactor: BossInteractorProtocol?
-    private let router: BossWireframeProtocol
-
-    init(interface: BossViewProtocol, interactor: BossInteractorProtocol?, router: BossWireframeProtocol) {
+    
+    init(interface: BossViewProtocol, interactor: BossInteractorProtocol?, router: BossRouterProtocol) {
         self.view = interface
         self.interactor = interactor
         self.router = router
     }
-
+    
+    func viewDidLoad(){
+        view?.showLoading()
+        interactor?.retrieveData()
+    }
+    
+    func viewDidRefresh(){
+        interactor?.retrieveData()
+    }
+    
+    func showDetailScreen(data: Boss) {
+        router?.showDetailScreen(view: view!, data: data)
+    }
+    
+    func didRetrieveData(data: [Boss]){
+        view?.hideLoading()
+        view?.showData(with: data)
+    }
+    
+    func onError() {
+        view?.hideLoading()
+        view?.showError()
+    }
+    
+    func isInternetConnected() -> Bool {
+        return (view?.isInternetConnected())!
+    }
+    
 }
