0078f58f0f35b2ff0b17c110f248b8c5c4da28d3
marcovazquez <marcovazquez77@hotmail.com>
2019-05-09 23:32:19 -0500

    Updated: Boss interacted added

--- yorha-client-ios/Modules/Bosses/BossInteractor.swift
+++ yorha-client-ios/Modules/Bosses/BossInteractor.swift
@@ -9,6 +9,59 @@
 import UIKit
 
 class BossInteractor: BossInteractorProtocol {
-
     weak var presenter: BossPresenterProtocol?
+    var remoteData: BossRemoteDataProtocol?
+    var localData: BossLocalDataProtocol?
+    
+    func onError() {
+        presenter?.onError()
+    }
+    
+    func retrieveData() {
+        do {
+            if let data = try localData?.retrieveBosses() {
+                let BossList: [Boss] = data.map() {
+                    let newZones: [Zone] = []
+                    print("zones: \($0.zones as Any)")
+                    return Boss(
+                        ID: Int($0.id),
+                        Name: $0.name!,
+                        Faction: $0.faction!,
+                        Zones: newZones,
+                        Photo: $0.photo!
+                    )
+                }
+                
+                if (presenter?.isInternetConnected())! || BossList.isEmpty {
+                    remoteData?.retrieveBosses()
+                } else {
+                    print("Local data for Bosss retrieved")
+                    presenter?.didRetrieveData(data: BossList)
+                }
+            } else {
+                remoteData?.retrieveBosses()
+            }
+            
+        } catch {
+            presenter?.didRetrieveData(data: [])
+        }
+    }
+    
+    func onPostsRetrieved(_ data: [Boss]) {
+        presenter?.didRetrieveData(data: data)
+        
+        /*for Boss in data {
+            do {
+                try localData?.storeBoss(
+                    id: Int32(Boss.ID),
+                    name: Boss.Name,
+                    faction: Boss.Faction,
+                    photo: Boss.Photo,
+                    zones: Boss.Zones
+                )
+            } catch {
+                
+            }
+        }*/
+    }
 }
