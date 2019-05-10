8158005482d29e7381a1aca38f88d596788974f5
marcovazquez <marcovazquez77@hotmail.com>
2019-05-10 16:34:52 -0500

    Created: Pod program entity added

---
+++ yorha-client-ios/Entities/PodProgram.swift
@@ -0,0 +1,17 @@
+//
+//  PodProgram.swift
+//  yorha-client-ios
+//
+//  Created by Marco Vazquez on 5/10/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import Foundation
+
+struct PodProgram: Codable {
+    var ID: Int
+    var Name: String
+    var Program: String
+    var Cooldown: Int
+    var Photo: String
+}
