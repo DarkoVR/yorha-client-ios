bbe246abab579d54f3b89a1e9427bb55ef0dd4bb
marcovazquez <marcovazquez77@hotmail.com>
2019-05-09 23:33:11 -0500

    Updated: Boss protocols added

--- yorha-client-ios/Modules/Bosses/BossProtocols.swift
+++ yorha-client-ios/Modules/Bosses/BossProtocols.swift
@@ -6,25 +6,58 @@
 //  Copyright © 2019 Marco Vazquez. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 //MARK: Wireframe -
-protocol BossWireframeProtocol: class {
-
+protocol BossRouterProtocol: class {
+    static func createModule() -> UIViewController
+    func showDetailScreen(view: BossViewProtocol, data: Boss)
 }
 //MARK: Presenter -
 protocol BossPresenterProtocol: class {
-
+    var view: BossViewProtocol? { get set }
+    var interactor: BossInteractorProtocol? { get set }
+    var router: BossRouterProtocol? { get set }
+    func viewDidLoad()
+    func viewDidRefresh()
+    func showDetailScreen(data: Boss)
+    func didRetrieveData(data: [Boss])
+    func onError()
+    func isInternetConnected() -> Bool
 }
 
 //MARK: Interactor -
 protocol BossInteractorProtocol: class {
-
-  var presenter: BossPresenterProtocol?  { get set }
+    var presenter: BossPresenterProtocol?  { get set }
+    var remoteData: BossRemoteDataProtocol?  { get set }
+    var localData: BossLocalDataProtocol?  { get set }
+    func retrieveData()
+    func onPostsRetrieved(_ data: [Boss])
+    func onError()
 }
 
 //MARK: View -
 protocol BossViewProtocol: class {
+    var presenter: BossPresenterProtocol?  { get set }
+    func showData(with data: [Boss])
+    func showError()
+    func showLoading()
+    func hideLoading()
+    func isInternetConnected() -> Bool
+}
 
-  var presenter: BossPresenterProtocol?  { get set }
+//Data Manager protocols
+protocol BossRemoteDataProtocol: class {
+    var interactor: BossInteractorProtocol?  { get set }
+    var presenter: BossPresenterProtocol?  { get set }
+    func retrieveBosses()
+    func retrieveOneBoss(data: Boss)
+    func storeBoss(data: Boss)
+    func updateBoss(data: Boss)
+    func deleteBoss(data: Boss)
+}
+
+protocol BossLocalDataProtocol: class {
+    func retrieveBosses() throws -> [BossCD]
+    func storeBoss(id: Int32, name: String, faction: String, photo: String, zones: [Zone]) throws
 }
