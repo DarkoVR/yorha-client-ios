86a3f3a4f7959c3c2738589ed34cd3dd15f62e3a
marcovazquez <marcovazquez77@hotmail.com>
2019-05-11 17:21:04 -0500

    Created: Weapon network session added

---
+++ yorha-client-ios/DataManager/Network/WeaponSession.swift
@@ -0,0 +1,105 @@
+//
+//  WeaponSession.swift
+//  yorha-client-ios
+//
+//  Created by Marco Vazquez on 5/11/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import Foundation
+
+class WeaponSession: WeaponRemoteDataProtocol {
+    var interactor: WeaponInteractorProtocol?
+    var presenter: WeaponPresenterProtocol?
+    
+    func retrieveWeapons() {
+        let petition = request(endpoint: Endpoints.weapons)
+        URLSession.shared.dataTask(with: petition) {
+            data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                self.interactor?.onError()
+                return
+            }
+            
+            if let jsonPetitions = try? JSONDecoder().decode([Weapon].self, from: data) {
+                print("petition made to: \(petition) \nwith method: \(petition.httpMethod ?? "") \nwith data: \(jsonPetitions)")
+                DispatchQueue.main.async {
+                    self.interactor?.onPostsRetrieved(jsonPetitions)
+                }
+            } else {
+                self.interactor?.onError()
+            }
+            }.resume()
+    }
+    
+    func retrieveOneWeapon(data: Weapon) {
+        let petition = request(endpoint: Endpoints.weapons, id: data.ID)
+        URLSession.shared.dataTask(with: petition) {
+            data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            if let jsonPetitions = try? JSONDecoder().decode(Weapon.self, from: data) {
+                print(jsonPetitions)
+            } else {
+                print("somenthing went wrong!")
+            }
+            }.resume()
+    }
+    
+    func storeWeapon(data: Weapon) {
+        guard let uploadData = try? JSONEncoder().encode(data) else {
+            return
+        }
+        
+        let petition = request(endpoint: Endpoints.weapons, method: Methods.post)
+        URLSession.shared.uploadTask(with: petition, from: uploadData) {
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Weapon stored!")
+            }.resume()
+    }
+    
+    func updateWeapon(data: Weapon) {
+        guard let uploadData = try? JSONEncoder().encode(data) else {
+            return
+        }
+        
+        let petition = request(endpoint: Endpoints.weapons, id: data.ID, method: Methods.put)
+        URLSession.shared.uploadTask(with: petition, from: uploadData) {
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Weapon updated!")
+            }.resume()
+    }
+    
+    func deleteWeapon(data: Weapon) {
+        let petition = request(endpoint: Endpoints.weapons, id: data.ID, method: Methods.delete)
+        URLSession.shared.dataTask(with: petition){
+            data, response, error in
+            
+            guard let _ = data, error == nil else {
+                print("error = \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            print("Weapon deleted!")
+            }.resume()
+    }
+}
+
